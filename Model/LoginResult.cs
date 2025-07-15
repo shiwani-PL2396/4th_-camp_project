1b13841c4585c9bc97ae02c44831ee88ac19b3d3
Nadiya-badusha <nadiyabadusha129@gmail.com>
2025-07-15 16:19:26 +0530

    Add files via upload

---
+++ Model/LoginResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS_Project.Model
+{
+    public class LoginResult
+    {
+        public int UserId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
---
+++ Model/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS_Project.Model
+{
+    public class UserRole
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
